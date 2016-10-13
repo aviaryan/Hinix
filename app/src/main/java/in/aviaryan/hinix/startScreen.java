64af66a70222278a3456901748daac90ac8ec1f1
rajukoushik <g.rajukoushik@gmail.com>
2016-10-13 17:15:32 +0530

    updated instructions

--- app/src/main/java/in/aviaryan/hinix/startScreen.java
+++ app/src/main/java/in/aviaryan/hinix/startScreen.java
@@ -69,8 +69,8 @@ public class startScreen extends AppCompatActivity {
                 // set dialog message
                 alertDialogBuilder
                         .setMessage("1. You are supposed to make as many words as you can from the given set of letters in the grid.\n" +
-                                "2. If the submitted word is a valid one then it it added to the list and the score would be updated.\n"+
-                                "3. Lastly you can challenge the game to show you all the possible words.\n\n");
+                                "2. If the submitted word is a valid one then it it added to the list and the score would be updated.\n" +
+                                "3. Lastly you can challenge the game to show you all the possible words.\n\n ");
 
                 AlertDialog alertDialog = alertDialogBuilder.create();
 
